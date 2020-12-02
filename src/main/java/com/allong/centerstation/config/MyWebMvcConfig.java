e3cb3876d392ecd29355f157814b187c84395078
AllongYangjian <952801974@qq.com>
2020-12-02 14:04:51 +0800

    1、处理打包失败问题 2、处理Controller问题

--- src/main/java/com/allong/centerstation/config/MyWebMvcConfig.java
+++ src/main/java/com/allong/centerstation/config/MyWebMvcConfig.java
@@ -18,11 +18,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyWebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("/index");
-        registry.addViewController("/index").setViewName("/index");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("/index");
+//        registry.addViewController("/index").setViewName("/index");
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
---
+++ src/main/java/com/allong/centerstation/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.allong.centerstation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Copyright (C), 2015-2020, 杭州奥朗信息科技有限公司
+ * FileName: IndexController
+ * Author:   YJ
+ * Date:     2020/12/2 14:00
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * YJ       2020/12/2 14:00        1.0              描述
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+    @RequestMapping("/index")
+    public String index2() {
+        return "index";
+    }
+}
