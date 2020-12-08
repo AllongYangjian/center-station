f8df82514a88225eb198a5bba0b2bf38e00b602f
AllongYangjian <952801974@qq.com>
2020-12-08 16:17:23 +0800

    添加登录界面

---
+++ src/main/resources/static/js/common/title.js
@@ -0,0 +1,8 @@
+document.write('<script src="/static/easyui/jquery.min.js"></script>');
+document.write('<script src="/static/easyui/jquery.easyui.min.js"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="/static/css/base.css"/>');
+// document.write('<script src="/static/js/common/common-utils.js"></script>');
+// document.write('<script src="/static/js/common/utils.js"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="/static/easyui/themes/icon.css"/>');
+document.write('<link rel="stylesheet" type="text/css" href="/static/easyui/themes/default/easyui.css"/>');
+document.write('<script type="text/javascript" src="/static/easyui/locale/easyui-lang-zh_CN.js"></script>');
