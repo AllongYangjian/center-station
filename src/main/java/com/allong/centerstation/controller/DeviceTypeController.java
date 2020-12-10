cd1453a76a311a75c76e9ea0fd8f5d7f3481da09
AllongYangjian <952801974@qq.com>
2020-12-10 17:38:59 +0800

    设备类型增删改查

--- src/main/java/com/allong/centerstation/controller/DeviceTypeController.java
+++ src/main/java/com/allong/centerstation/controller/DeviceTypeController.java
@@ -1,9 +1,13 @@
 package com.allong.centerstation.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.allong.centerstation.common.Result;
+import com.allong.centerstation.domain.DeviceType;
+import com.allong.centerstation.service.DeviceTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,8 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-12-09
  */
 @RestController
-@RequestMapping("/deviceType")
+@RequestMapping("/api/device")
 public class DeviceTypeController {
+    @Autowired
+    private DeviceTypeService deviceTypeService;
 
+    @GetMapping
+    public ResponseEntity<Object> list() {
+        return new ResponseEntity<>(new Result.Builder<>().setData(deviceTypeService.list()).buildQuerySuccess(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody DeviceType deviceType) {
+        return new ResponseEntity<>(new Result.Builder<>().setData(deviceType.insert()).buildSaveSuccess(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> update(@RequestBody DeviceType deviceType) {
+        return new ResponseEntity<>(new Result.Builder<>().setData(deviceType.updateById()).buildUpdateSuccess(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{did}")
+    public ResponseEntity<Object> delete(@PathVariable("did") Integer did) {
+        return new ResponseEntity<>(new Result.Builder<>().setData(deviceTypeService.removeById(did)).buildDeleteSuccess(), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Object> deleteAll() {
+        return new ResponseEntity<>(new Result.Builder<>().setData(deviceTypeService.remove(null)).buildDeleteSuccess(), HttpStatus.OK);
+    }
 }
 
---
+++ src/main/resources/static/js/system/device.js
@@ -0,0 +1,273 @@
+const columns = [[
+    {
+        field: 'ck',
+        checkbox: true
+    },
+    {
+        title: 'ID',
+        field: 'id',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '名称',
+        field: 'name',
+        align: 'center',
+        width: 1
+    }, {
+        title: '制造商',
+        field: 'manufacturer',
+        align: 'center',
+        width: 1
+    }, {
+        title: '品牌',
+        field: 'brand',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '型号',
+        field: 'model',
+        align: 'center',
+        width: 1
+    }
+]];
+
+var keyColumns = [[
+    {
+        field: 'ck',
+        checkbox: true
+    },
+    {
+        title: 'ID',
+        field: 'id',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '代码',
+        field: 'code',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '名称',
+        field: 'name',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '颜色',
+        field: 'keyColor',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '大小',
+        field: 'keySize',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '下限',
+        field: 'min',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '上限',
+        field: 'max',
+        align: 'center',
+        width: 1
+    },
+
+    {
+        title: '索引',
+        field: 'position',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '是否存在波形',
+        field: 'wave',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '缩放系数',
+        field: 'scale',
+        align: 'center',
+        width: 1
+    }
+]];
+
+
+var $deviceTable;
+var currentItem;
+
+$(function () {
+    initDeviceTable();
+
+    $("#add").on('click', () => {
+        currentItem = undefined;
+        $deviceTable.datagrid('uncheckAll');
+        showDeviceDialog();
+    });
+
+    $("#edit").on('click', () => {
+        if (currentItem) {
+            showDeviceDialog();
+        } else {
+            showToast('提示', '请选择要修改的数据');
+        }
+    });
+
+    $("#delete").on('click', () => {
+        if (currentItem) {
+            showDeleteToast(doDeleteDeviceRecord);
+        } else {
+            showToast('提示', '请选择要删除的数据');
+        }
+    });
+
+    $("#query").on('click', () => {
+        loadDeviceData();
+    });
+
+    $("#device_save").on('click', () => {
+        saveDeviceInfo();
+    });
+
+    loadDeviceData();
+});
+
+
+/**
+ * 初始化用户信息表单
+ */
+function bindDeviceFormData() {
+    if (currentItem) {
+        $("#id").textbox('setValue', currentItem.id);
+        $("#name").textbox('setValue', currentItem.name);
+        $("#manufacturer").textbox('setValue', currentItem.manufacturer);
+        $("#brand").textbox('setValue', currentItem.brand);
+        $("#model").textbox('setValue', currentItem.model);
+    }
+}
+
+/**
+ * 重置用户信息表单
+ */
+function resortDeviceFormData() {
+    // $("#ff")[0].reset(); //并没有删除原来的值
+    $("#id").textbox('setValue', '');
+    $("#name").textbox('setValue', '');
+    $("#manufacturer").textbox('setValue', '');
+    $("#brand").textbox('setValue', '');
+    $("#model").textbox('setValue', '');
+}
+
+/**
+ * 显示用户信息表单对话框
+ */
+function showDeviceDialog() {
+    $("#device_dialog").dialog({
+        onOpen: () => {
+            bindDeviceFormData();
+        },
+        onClose: () => {
+            resortDeviceFormData();
+        }
+    });
+    $("#device_dialog").dialog('open');
+}
+
+/**
+ * 初始化用户信息
+ */
+function initDeviceTable() {
+    $deviceTable = $("#device_list");
+    $deviceTable.datagrid({
+        rownumbers: true,
+        showFooter: true,
+        fitColumns: true,
+        pagination: true,
+        remoteSort: false,
+        singleSelect: true,
+        pagePosition: 'bottom',
+        pageNumber: 1,
+        pageSize: 20,
+        columns: columns,
+        onCheck: (index, data) => {
+            currentItem = data;
+        },
+        onUncheck: (index, data) => {
+            currentItem = undefined;
+        }
+    });
+}
+
+/**
+ * 记载用户信息
+ */
+function loadDeviceData() {
+    $.ajax({
+        url: "/api/device",
+        type: 'get',
+        dataType: 'json',
+        success: data => {
+            if (data.code === 200) {
+                $deviceTable.datagrid({data: data.data});
+            }
+        }
+    })
+}
+
+function saveDeviceInfo() {
+    let form = $("#ff");
+    if (form.form('validate')) {
+        let data = form.serializeObject();
+        // console.log(data);
+        if (data.id) {
+            doSaveOrUpdateDeviceInfo(data, 'PUT');
+        } else {
+            doSaveOrUpdateDeviceInfo(data, 'POST');
+        }
+
+    } else {
+        showToast('警告', '存在校验未通过项目');
+    }
+}
+
+function doSaveOrUpdateDeviceInfo(data, method) {
+    $.ajax({
+        url: '/api/device',
+        type: method,
+        data: JSON.stringify(data),
+        dataType: 'json',
+        contentType: 'application/json',
+        success: (data) => {
+            showToast('提示', data.message);
+            if (data.code === 200) {
+                loadDeviceData();
+                resortDeviceFormData();
+            }
+        }
+    })
+}
+
+function doDeleteDeviceRecord() {
+    $.ajax({
+        url: '/api/device/' + currentItem.id,
+        type: 'DELETE',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: (data) => {
+            showToast('提示', data.message);
+            if (data.code === 200) {
+                loadDeviceData();
+            }
+        }
+    })
+}
+
