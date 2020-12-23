573dc301fc57081863584d5a39c1d54940b23ccd
AllongYangjian <952801974@qq.com>
2020-12-23 17:00:15 +0800

    医院床位增删改查

--- src/main/java/com/allong/centerstation/controller/HospitalBedController.java
+++ src/main/java/com/allong/centerstation/controller/HospitalBedController.java
@@ -1,9 +1,15 @@
 package com.allong.centerstation.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.allong.centerstation.common.Result;
+import com.allong.centerstation.domain.entity.HospitalBed;
+import com.allong.centerstation.logger.annotation.Log;
+import com.allong.centerstation.service.HospitalBedService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,8 +20,59 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-12-23
  */
 @RestController
-@RequestMapping("/hospitalBed")
+@RequestMapping("/api/bed")
 public class HospitalBedController {
+
+    @Autowired
+    private HospitalBedService bedService;
+
+    @GetMapping
+    @Log("查询床位列表")
+    public ResponseEntity<Object> list(){
+        return new ResponseEntity<>(new Result.Builder<>().setData(bedService.list()).buildQuerySuccess(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{hid}")
+    @Log("查询指定医院的床位列表")
+    public ResponseEntity<Object> listByHid(@PathVariable("hid")Integer hid){
+        return new ResponseEntity<>(new Result.Builder<>().setData(bedService.list(new QueryWrapper<HospitalBed>()
+                .eq("hid",hid))).buildQuerySuccess(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    @Log("保存床位信息")
+    public ResponseEntity<Object> save(@RequestBody HospitalBed bed){
+        boolean result = bed.insert();
+        if(result){
+            return new ResponseEntity<>(new Result.Builder<>().setData(true).buildSaveSuccess(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(new Result.Builder<>().setData(false).buildSaveFailed(), HttpStatus.OK);
+        }
+
+    }
+
+    @PutMapping
+    @Log("更新床位信息")
+    public ResponseEntity<Object> update(@RequestBody HospitalBed bed){
+        boolean result = bed.updateById();
+        if(result){
+            return new ResponseEntity<>(new Result.Builder<>().setData(true).buildUpdateSuccess(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(new Result.Builder<>().setData(false).buildUpdateFailed(), HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Log("更新床位信息")
+    public ResponseEntity<Object> delete(@PathVariable("id")Integer id){
+        boolean result = bedService.removeById(id);
+        if(result){
+            return new ResponseEntity<>(new Result.Builder<>().setData(true).buildDeleteSuccess(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(new Result.Builder<>().setData(false).buildDeleteFailed(), HttpStatus.OK);
+        }
+    }
+
 
 }
 
--- src/main/resources/static/js/system/hospital.js
+++ src/main/resources/static/js/system/hospital.js
@@ -29,12 +29,43 @@ var columns = [[
     }
 ]];
 
+var bedColumns = [[
+    {
+        title: 'ID',
+        field: 'id',
+        align: 'center',
+        width: 1,
+        hidden:true
+    },
+    {
+        title: '医院',
+        field: 'hid',
+        align: 'center',
+        width: 1
+    },
+    {
+        title: '床号',
+        field: 'bed',
+        align: 'center',
+        width: 1
+    }
+]];
+
 var $hospitalTable;
-var currentItem;
+var $bedTable;
+var currentItem,currentBedItem;
 
 $(function () {
     initHospitalTable();
+    initHospitalToolbar();
+    loadHospitalData();
 
+    initBedListTable();
+    initBedListTableToolbar();
+});
+
+
+function initHospitalToolbar() {
     $("#add").on('click', () => {
         currentItem = undefined;
         $hospitalTable.datagrid('uncheckAll');
@@ -68,9 +99,7 @@ $(function () {
     $("#hospital_close").on('click', () => {
         $("#hospital_dialog").dialog('close');
     });
-
-    loadHospitalData();
-});
+}
 
 function bindHospitalFormData() {
     if (currentItem) {
@@ -102,6 +131,7 @@ function showHospitalDialog() {
 }
 
 
+
 function initHospitalTable() {
     $hospitalTable = $("#hospital_list");
     $hospitalTable.datagrid({
@@ -117,12 +147,115 @@ function initHospitalTable() {
         columns: columns,
         onCheck: (index, data) => {
             currentItem = data;
+            //查找医院床位信息
+            queryHospitalBedByHid();
         },
         onUncheck: (index, data) => {
             currentItem = undefined;
         },
         loadFilter: pagerFilter
     });
+}
+
+/**
+ * 查找医院下属床位列表
+ * @param id
+ */
+function queryHospitalBedByHid() {
+    let url;
+    if(currentItem){
+        url = '/api/bed/'+currentItem.id;
+    }else {
+        url = '/api/bed';
+    }
+    $.ajax({
+        url:url,
+        type:'get',
+        dataType:'json',
+        success:data=>{
+            $bedTable.datagrid({data:data.data});
+        },
+        error:errorHandler
+    })
+}
+
+function initBedListTable() {
+    $bedTable = $("#bed_list");
+    $bedTable.datagrid({
+        rownumbers: true,
+        showFooter: true,
+        fitColumns: true,
+        pagination: true,
+        remoteSort: false,
+        singleSelect: true,
+        pagePosition: 'bottom',
+        pageNumber: 1,
+        pageSize: 20,
+        columns: bedColumns,
+        onCheck: (index, data) => {
+            currentBedItem = data;
+        },
+        onUncheck: (index, data) => {
+            currentBedItem = undefined;
+        },
+        loadFilter: pagerFilter
+    });
+}
+
+function initBedListTableToolbar() {
+    $("#bed_add").on('click', () => {
+        if(currentItem ===undefined){
+            showToast('提示','请先选择医院');
+            return;
+        }
+        currentBedItem = undefined;
+        showBedDialog();
+    });
+
+    $("#bed_edit").on('click', () => {
+        if (currentBedItem) {
+            showBedDialog();
+        } else {
+            showToast('提示', '请选择要修改的数据');
+        }
+    });
+
+    $("#bed_delete").on('click', () => {
+        if (currentBedItem) {
+            showDeleteToast(deleteBedItem);
+        } else {
+            showToast('提示', '请选择要删除的数据');
+        }
+    });
+
+    $("#bed_query").on('click', () => {
+        queryHospitalBedByHid();
+    });
+
+    $("#bed_save").on('click',function () {
+        saveOrUpdateBedInfo();
+    });
+
+    $("#bed_close").on('click',function () {
+        $("#bed_ff").dialog('close');
+    });
+}
+
+function showBedDialog() {
+    $("#bed_ff").dialog({
+        title:'床位信息',
+        onOpen:function () {
+            if(currentBedItem){
+                $("#bed_id").textbox('setValue',currentBedItem.id);
+                $("#bed_name").textbox('setValue',currentBedItem.bed);
+            }
+        },
+        onClose:function () {
+            $("#bed_id").textbox('setValue','');
+            $("#bed_name").textbox('setValue','');
+        }
+    });
+    $("#bed_ff").dialog('open');
 }
 
 
@@ -184,6 +317,58 @@ function doDeleteHospitalRecord() {
             showToast('提示', data.message);
             if (data.code === 200) {
                 loadHospitalData();
+            }
+        },
+        error:errorHandler
+    })
+}
+
+function saveOrUpdateBedInfo() {
+    let form = $("#bed_ff");
+    if (form.form('validate')) {
+        let data = form.serializeObject();
+        // console.log(data);
+        if (data.id) {
+            doSaveOrUpdateBedInfo(data, 'PUT');
+        } else {
+            doSaveOrUpdateBedInfo(data, 'POST');
+        }
+
+    } else {
+        showToast('警告', '存在校验未通过项目');
+    }
+}
+
+function doSaveOrUpdateBedInfo(data,method) {
+    data.hid = currentItem.id;
+    $.ajax({
+        url: '/api/bed',
+        type: method,
+        data:JSON.stringify(data),
+        dataType: 'json',
+        contentType: 'application/json',
+        success: (data) => {
+            showToast('提示', data.message);
+            if (data.code === 200) {
+                queryHospitalBedByHid();
+                $("#bed_id").textbox('setValue','');
+                $("#bed_name").textbox('setValue','');
+            }
+        },
+        error:errorHandler
+    })
+}
+
+function deleteBedItem() {
+    $.ajax({
+        url: '/api/bed/'+currentBedItem.id,
+        type: 'delete',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: (data) => {
+            showToast('提示', data.message);
+            if (data.code === 200) {
+                queryHospitalBedByHid();
             }
         },
         error:errorHandler
